3406b6a58a8d7ea6fae9f1090b2b4a3533b71829
Nurdin Ismail <nurdin.ismail@student.moringaschool.com>
2023-09-24 16:03:11 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
+from sqlalchemy import desc,asc
 
 from models import db, Message
 
@@ -14,13 +15,88 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods = ['GET','POST'])
 def messages():
-    return ''
+    
+    
+    messages = Message.query.order_by(asc(Message.created_at))
 
-@app.route('/messages/<int:id>')
+    if request.method == 'GET':
+        messages_list=[]
+        for message in messages:
+            message_dict=message.to_dict()
+            messages_list.append(message_dict)
+
+        response = make_response(jsonify(messages_list),200)
+
+        return response
+    elif request.method == 'POST':
+        requesto = request.get_json()
+        new_message = Message(
+            body = requesto.get("body"),
+            username = requesto.get("username"),
+            
+            
+        )
+        
+        
+        db.session.add(new_message)
+        db.session.commit()
+
+        message_dict = new_message.to_dict()
+
+        response = make_response(
+            jsonify(message_dict),
+            201
+        )
+
+        return response
+
+@app.route('/messages/<int:id>', methods = ['GET', 'PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id = id).first()
+    
+    if request.method == 'GET':
+        message_dict=message.to_dict()
+       
+
+        response = make_response(jsonify(message_dict),200)
+
+        return response
+    
+    elif request.method == 'PATCH':
+        requesto = request.get_json()
+        
+        for attr in requesto:
+            setattr(message, attr, requesto.get(attr))
+            
+        db.session.add(message)
+        db.session.commit()
+        
+        message_dict= message.to_dict()
+        
+        response = make_response(
+                jsonify(message_dict),
+                200
+            )
+
+        return response
+    
+    elif request.method == 'DELETE':
+         db.session.delete(message)
+         db.session.commit()
+
+         response_body = {
+                "delete_successful": True,
+                "message": "Message deleted."    
+            }
+
+         response = make_response(
+                jsonify(response_body),
+                200
+            )
+
+         return response
 
 if __name__ == '__main__':
     app.run(port=5555)
